03ab526f543a35286b4017be366c108abae6f5c0
yui <poo.net951@gmail.com>
2012-07-11 16:48:26 +0700

    New HelloWorld

---
+++ src/th/ac/nu/students/u54347729/compro/ch01/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.students.u54347729.compro.ch01;
+public class HelloWorld {
+    public static void main (String[]args){
+        System.out.print("Hello World");
+    }
+    
+}
