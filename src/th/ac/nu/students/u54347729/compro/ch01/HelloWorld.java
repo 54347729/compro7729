f6bbdd7b54851ad7b1aa4d3a292e8695e91887d5
yui <poo.net951@gmail.com>
2012-07-11 16:53:10 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/students/u54347729/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347729/compro/ch01/HelloWorld.java
@@ -5,7 +5,8 @@
 package th.ac.nu.students.u54347729.compro.ch01;
 public class HelloWorld {
     public static void main (String[]args){
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Programming is fun");
     }
     
 }
